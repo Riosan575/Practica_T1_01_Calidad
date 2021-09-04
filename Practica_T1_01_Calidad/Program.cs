ec115cf85c67c0de738f11dd40ec306f0241f6c6
Riosan575 <an_99_ben@hotmail.com>
2021-09-03 19:22:56 -0500

    Practica_T1

---
+++ Practica_T1_01_Calidad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica_T1_01_Calidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
